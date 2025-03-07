a67cff8061d172e9ea65197f9563176c814da7e7
Abdulkareem Siddiq <73565559+abdulkareem-siddiq@users.noreply.github.com>
2025-03-07 08:19:50 -0800

    Bugfix/number mismatch on mobile (#2)
    * Implemented optimization using sharding of the number data. Single digit prefix.
    
    * Fixed the fuzz by calling them individually
    
    * Updated the fuzz to skip invalid input

--- number-parser_test.go
+++ number-parser_test.go
@@ -1,22 +1,108 @@
 package numberparser
 
 import (
+	_ "embed"
+	"fmt"
+
 	"strings"
 	"testing"
 )
 
-func TestPhoneNumberDataCsv_NotEmpty(t *testing.T){
-	if len(PhoneNumberDataCsv) == 0  {
+func TestPhoneNumberDataCsv_NotEmpty(t *testing.T) {
+	if len(PhoneNumberDataCsv) == 0 {
 		t.Errorf("PhoneNumberDataCsv is empty!")
 	}
 }
 
-func TestPhoneNumberData_Parsed(t *testing.T){
-	if len(PhoneNumberData) == 0  {
+func TestPhoneNumberData_Parsed(t *testing.T) {
+	if len(PhoneNumberData) == 0 {
 		t.Errorf("PhoneNumberData is empty!")
 	}
 }
 
+func TestPhoneNumberData_Invalid(t *testing.T) {
+	res := FindNumberDataForE164("+21012345")
+	if res != nil {
+		t.Errorf("PhoneNumberData must be empty for invalid number!")
+	}
+}
+
+func TestPhoneNumberData_UKMobile(t *testing.T) {
+	res := FindNumberDataForE164("+447762000000")
+	if res != nil && !res.IsMobile {
+		t.Errorf("PhoneNumberData must be mobile! %v", res)
+	}
+}
+
+func TestPhoneNumberData_UKMobile_old(t *testing.T) {
+	res := FindNumberDataForE164v0("+447762000000")
+	if res != nil && !res.IsMobile {
+		t.Errorf("PhoneNumberData must be mobile! %v", res)
+	}
+}
+
+func TestPhoneNumberData_UKMobile_old_2(t *testing.T) {
+	res := FindNumberDataForE164v0("+447762987654")
+	if res != nil && !res.IsMobile {
+		t.Errorf("PhoneNumberData must be mobile! %v", res)
+	}
+}
+
+func TestPhoneNumberData_Satellite(t *testing.T) {
+	testcases := []struct {
+		input string
+		want  bool
+	}{
+		{"+87810", true},
+		{"+614899", true},
+		{"+4312345", false},
+	}
+
+	for _, tc := range testcases {
+		res := FindNumberDataForE164(tc.input)
+		if res.IsSatellite != tc.want {
+			t.Errorf("FindNumberDataForE164: %s in: %v   want: %v", tc.input, res.IsSatellite, tc.want)
+		}
+	}
+}
+
+func TestPhoneNumberData_Mobile(t *testing.T) {
+	testcases := []struct {
+		input string
+		want  bool
+	}{
+		{"+447762000000", true},
+		{"+5492362336", true},
+		{"+14159991111", false},
+		{"+443111111", false},
+	}
+
+	for _, tc := range testcases {
+		res := FindNumberDataForE164(tc.input)
+		if res.IsMobile != tc.want {
+			t.Errorf("FindNumberDataForE164: %s in: %v   want: %v", tc.input, res.IsMobile, tc.want)
+		}
+	}
+}
+
+func TestPhoneNumberData_Geographic(t *testing.T) {
+	testcases := []struct {
+		input string
+		want  bool
+	}{
+		{"+441387", true},
+		{"+4420000", true},
+		{"+1415", true},
+		{"+13102223333", true},
+	}
+
+	for _, tc := range testcases {
+		res := FindNumberDataForE164(tc.input)
+		if res.IsGeographic != tc.want {
+			t.Errorf("FindNumberDataForE164: %s in: %v   want: %v", tc.input, res.IsGeographic, tc.want)
+		}
+	}
+}
 
 func TestNormalizeE164(t *testing.T) {
 	testcases := []struct {
@@ -45,6 +131,58 @@ func FuzzNormalizeE164(f *testing.F) {
 	f.Fuzz(func(t *testing.T, arg string) {
 		res := NormalizeE164(arg)
 		if !strings.HasPrefix(res, "+") {
+			t.Errorf("NormalizeE164 fails %s --> %s", arg, res)
+		}
+	})
+}
+
+func TestSanitizeNumber(t *testing.T) {
+	tests := []struct {
+		phone string
+		want  string
+	}{
+		{"+18005551212", "18005551212"},
+		{"+18005551212x,,099", "18005551212x,,099"},
+		{"1 (800) 555-1212", "18005551212"},
+		{" 1 (800) 555-1212", "18005551212"},
+		{"018005551212", "18005551212"},
+	}
+
+	for _, tt := range tests {
+		got := SanitizeNumber(tt.phone)
+		if got != tt.want {
+			t.Errorf("SanitizeNumber() = %v, want %v", got, tt.want)
+		}
+		t.Logf("SanitizeNumber(%v) = %v, want %v", tt.phone, got, tt.want)
+	}
+}
+
+func FuzzSanitizeNumber(f *testing.F) {
+	testcases := []string{"18005551212",
+		"+18005551212",
+		" 18005551212",
+		"08005551212",
+		"+18005551212x,,099",
+		"1 (800) 555-1212",
+		" 1 (800) 555-1212",
+	}
+
+	for _, tc := range testcases {
+		f.Add(tc)
+	}
+
+	f.Fuzz(func(t *testing.T, arg string) {
+
+		res := SanitizeNumber(arg)
+
+		// This minor update will allow us to bypass input that is
+		// too small to be a phone number causing a needless failure
+		// during fuzzing.
+		if len(res) < 6 {
+			t.Skip(fmt.Sprintf("Skipping res:`%s`; too small.", res))
+		}
+
+		if len(arg) != 0 && len(res) == 0 {
 			t.Errorf("NormalizeE164 fails %s --> %s", arg, res)
 		}
 	})
